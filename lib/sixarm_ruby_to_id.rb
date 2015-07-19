ab789e34b51e9aa4425bdd4d5b2929a9f9a63e9a
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-18 20:32:13 -0600

    Update from FILE to require_relative

--- lib/sixarm_ruby_to_id.rb
+++ lib/sixarm_ruby_to_id.rb
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 ['array','date','hash','nil','numeric','object','string'].map{|x|
-  require File.dirname(__FILE__) + "/sixarm_ruby_to_id/#{x}.rb"
+  require_relative "sixarm_ruby_to_id/#{x}"
 }
